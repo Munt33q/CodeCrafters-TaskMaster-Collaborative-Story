eda940f412531a8afbbe4a798917e2b31f0815a9
Safak Yamali <safak@analyticsandsociety.com>
2025-09-18 22:24:37 -0500

    Added Chapter 2: The Travel

---
+++ chapter_2.py
@@ -0,0 +1,11 @@
+# Sir Git and the cursor mounted their branchtravel horses.
+# They galloped at immense speed, shattering spacetime.
+# Before them, the void stretched on, vast and empty.
+# At last, they arrived at the new branch.
+# To their surprise, nothing had changed.
+
+def chapter2():
+    print("'Soon,' one said, 'this void will be full of commits.'")
+    print("'We must be the first to create this branch,' the other replied.")
+
+chapter2()
